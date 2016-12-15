5fe7e38005b9de4ebc9fd4a36f8eeb754911881b
asirotenko <asirotenko@admins-MacBook-Pro.local>
2016-12-14 22:12:04 -0800

    Header icons and custom section header view, gif file refleting widgets appearance

---
+++ Accordion/SectionHeader.swift
@@ -0,0 +1,19 @@
+//
+//  SectionHeader.swift
+//  Accordion
+//
+//  Created by asirotenko on 12/14/16.
+//  Copyright © 2016 asirotenko. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeader: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var img_down: UIImageView!
+    
+    @IBOutlet weak var img_right: UIImageView!
+    
+}
